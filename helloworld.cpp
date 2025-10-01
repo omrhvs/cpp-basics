3ea0bc8bec3d418216421667a1f95a91d63c86ba
Omar Venegas <omrhvs@gmail.com>
2025-10-01 02:00:02 -0600

    learning datatypes, declarations and value assiganations in C++

--- helloworld.cpp
+++ helloworld.cpp
@@ -12,5 +12,50 @@ int main() {
     std::cout << "Hello, World!" << std::endl;
     std::cout << "My name is Omar." << std::endl;
 
+    // Variable declaration
+    int x;
+    // Variable assignation
+    x = 27;
+    std::cout << x << '\n';
+
+    // Variable declaration and assignation
+    int y = 3;
+    std::cout << y << '\n';
+
+    int sum = x + y;
+    std::cout << sum << '\n';
+
+    /*
+        Data types in C++:
+    */
+
+    // Integers (whole numbers)
+    int age = 22;
+    std::cout << "Age: " << age << '\n';
+    
+    // String (text)
+    std::string name = "Omar";
+    std::cout << "Name: " << name << '\n';  
+    
+    // Floating point numbers (decimal numbers)
+    float gpa = 3.5;
+    std::cout << "GPA: " << gpa << '\n';
+    
+    // Double (more precise floating point numbers)
+    double gpa2 = 3.5555555555;
+    std::cout << "GPA2: " << gpa2 << '\n';
+    
+    // Character (single letters and symbols)
+    char grade = 'A';
+    std::cout << "Grade: " << grade << '\n';
+    
+    // Boolean (true or false)
+    bool isMale = true;
+    std::cout << "isMale: " << isMale << '\n';
+    
+    std::cout << name << " is " << age << " years old. He has a GPA of " << gpa << ", " <<  gpa2 << " specifically. His Grade is " << grade << '.\n';
+
+
+
     return 0;
 }
