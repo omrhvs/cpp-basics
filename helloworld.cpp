8a3f167400807ee74e670af7b3c2b3c3f7836d52
Omar Venegas <omrhvs@gmail.com>
2025-10-01 01:46:37 -0600

    trying basic syntax

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
 
 int main() {
-    
+
+    // This is a comment
+    /*
+         This is a 
+         multi-line
+         comment
+    */
+
+    std::cout << "Hello, World!" << std::endl;
+    std::cout << "My name is Omar." << std::endl;
+
     return 0;
 }
